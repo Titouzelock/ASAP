7f2eba7ec168ec5e8b0b143c008ceacb685db06a
Tristan Le Lievre <titouzerooki@gmail.com>
2025-11-01 16:14:15 +0200

    chore: add Unity library locally and minimal config for native tests

---
+++ lib/Unity/src/unity_config.h
@@ -0,0 +1,9 @@
+#ifndef UNITY_CONFIG_H
+#define UNITY_CONFIG_H
+
+// Optionally silence output when testing on embedded
+#define UNITY_OUTPUT_COLOR
+#define UNITY_INCLUDE_CONFIG_H
+#define UNITY_EXCLUDE_SETJMP_H
+
+#endif // UNITY_CONFIG_H
--- test/native_basic/test_main.cpp
+++ test/native_basic/test_main.cpp
@@ -4,8 +4,10 @@ void test_addition(void) {
     TEST_ASSERT_EQUAL(2, 1 + 1);
 }
 
-int main(int argc, char **argv) {
+void setup() {
     UNITY_BEGIN();
     RUN_TEST(test_addition);
-    return UNITY_END();
+    UNITY_END();
 }
+
+void loop() {}
